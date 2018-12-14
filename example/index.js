96205ef7c0407105b06fc4b9c0b63438e2135904
360BUYAD\wangyue92 <wangyue92@jd.com>
2018-12-14 15:28:45 +0800

    webpack 插件提交

--- example/index.js
+++ example/index.js
@@ -1,19 +1,30 @@
+let smockCore=require('smock-core');
+console.log(smockCore,'smockCore');
+
 class HelloWorldPlugin {
     constructor(options) {
       this.options = options;
     }
   
     apply(compiler) {
+      
+
         console.log(compiler.hooks);
         if(compiler.hooks){
           compiler.hooks.done.tap('HelloWorldPlugin', () => {
             console.log('Hello World!');
             console.log(this.options);
+            smockCore.init(
+              this.options
+            ); 
           });
         }else{
           compiler.plugin('done',() => {
             console.log('Hello World!');
             console.log(this.options);
+            smockCore.init(
+              this.options
+            ); 
           })
         }
     }
